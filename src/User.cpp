0863ab4713bb952e24393655fd157b644c63ca64
Clément Lagier <clement.lagier@epitech.eu>
2025-06-05 20:42:08 +0900

    [UPDATE] Added a getUuid and a borrowBook method for the User

--- src/User.cpp
+++ src/User.cpp
@@ -1,18 +1,18 @@
 #include "User.hpp"
 
-User::User(const std::string &name, uuid_t uuid, std::vector<std::string> borrowedBooksISBNs)
-  : _name(name), _borrowedBooksISBNs(borrowedBooksISBNs)
+User::User(const std::string &name)
+  : _name(name)
 {
-  std::memcpy(_uuid, uuid, sizeof(uuid_t));
-  Logging::Log("User created: " + _name + " with UUID: " + getUuid());
+  uuid_generate(_uuid);
+  Logging::Log("User created: " + _name + " with UUID: " + getStringUuid());
 }
 
 User::~User()
 {
-  Logging::Log("User destroyed: " + _name + " with UUID: " + getUuid());
+  Logging::Log("User destroyed: " + _name + " with UUID: " + getStringUuid());
 }
 
-std::string User::getUuid() const
+std::string User::getStringUuid() const
 {
   char uuidStr[37];
   uuid_unparse(_uuid, uuidStr);
@@ -20,13 +20,31 @@ std::string User::getUuid() const
   return std::string(uuidStr);
 }
 
+std::array<uint8_t, 16> User::getUuid() const
+{
+  std::array<uint8_t, 16> uuidCopy;
+  std::memcpy(uuidCopy.data(), _uuid, 16);
+  return uuidCopy;
+}
+
+std::string User::getName() const
+{
+  return _name;
+}
+
 void User::display() const
 {
-  Logging::Log("Displaying user: " + _name + " with UUID: " + getUuid());
+  Logging::Log("Displaying user: " + _name + " with UUID: " + getStringUuid());
   std::cout << "User Name: " << _name << std::endl;
-  std::cout << "User UUID: " << getUuid() << std::endl;
+  std::cout << "User UUID: " << getStringUuid() << std::endl;
   std::cout << "Borrowed Books ISBNs: " << std::endl;
   for (const auto &isbn : _borrowedBooksISBNs) {
     std::cout << "- " << isbn << std::endl;
   }
+}
+
+void User::borrowBook(const std::string &isbn)
+{
+  _borrowedBooksISBNs.push_back(isbn);
+  Logging::Log("Book with ISBN: " + isbn + " borrowed by user: " + _name);
 }
