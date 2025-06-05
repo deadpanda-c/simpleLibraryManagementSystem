d26b87e543a6b3ebd9eb70b2439d4523a59b8903
Clément Lagier <clement.lagier@epitech.eu>
2025-06-05 20:43:14 +0900

    [UPDATE] Updated the main.cpp for the final project

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
-#include "User.hpp"
+#include "Library.hpp"
 
 int main(void)
 {
   try {
-    uuid_t userId;
-    std::vector<std::string> books = {"1984", "Brave New World", "Fahrenheit 451"};
+    Library library;
 
-    uuid_generate(userId);
+    library.addBook(std::shared_ptr<Book>(new Book("1984", "George Orwell", "1234-5678", false, "")));
+    library.addBook(std::shared_ptr<Book>(new Book("To Kill a Mockingbird", "Harper Lee", "2345-6789", false, "")));
+    library.addBook(std::shared_ptr<Book>(new Book("The Great Gatsby", "F. Scott Fitzgerald", "3456-7890", false, "")));
 
-    User user1("Toto", userId, books);
+    library.addUser(std::shared_ptr<User>(new User("Toto")));
+    library.addUser(std::shared_ptr<User>(new User("Tata")));
+    library.addUser(std::shared_ptr<User>(new User("Titi")));
 
-    user1.display();
+    std::vector<std::array<uint8_t, 16>> userUuids = library.getUserUuids();
+    std::vector<std::string> bookisbns = library.getBooks();
+
+    // borrow books
+    for (size_t i = 0; i < userUuids.size() && i < bookisbns.size(); ++i) {
+      uuid_t userUuid;
+      std::copy(userUuids[i].begin(), userUuids[i].end(), userUuid);
+      library.borrowBook(userUuid, bookisbns[i]);
+    }
+
+    library.displayUsers();
+    library.displayBooks();
   } catch (const std::exception& e) {
     Logging::LogError("An error occurred: " + std::string(e.what()));
   }
